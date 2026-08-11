529f9b9bac18f07f3804ea6082b3b0fda5fb430c
Ashish Sharma <ashy07sharma@gmail.com>
2026-08-11 23:45:09 +0530

    Initial config

---
+++ src/main/java/com/projects/webhookdeliveryservice/WebhookDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.projects.webhookdeliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhookDeliveryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/webhookdeliveryservice/WebhookDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.webhookdeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
